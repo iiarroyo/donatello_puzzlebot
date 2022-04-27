ae417cc9d745c8094ca82553b177ad2b5a0b9f88
iiarroyo <israel.iap@outlook.com>
2022-04-27 18:35:06 -0500

    Erode works

--- src/read_img.py
+++ src/read_img.py
@@ -8,12 +8,9 @@ from sensor_msgs.msg import Image
 
 class Observer():
     def __init__(self):
-        # subscriber image
-        self.image = np.zeros((0, 0))
-        # processed image
-        self.p_img = np.zeros((0, 0))
-        # cv_bridge
-        self.bridge = cv_bridge.CvBridge()
+        self.image = np.zeros((0, 0))  # subscriber image
+        self.p_img = np.zeros((0, 0))  # processed image
+        self.bridge = cv_bridge.CvBridge()  # cv_bridge
         # rospy.Subscriber("/camera/image_raw", Image, self.img_cb)
         rospy.Subscriber("/usb_cam/image_raw", Image, self.img_cb)
         # image publisher
@@ -24,32 +21,35 @@ class Observer():
         print("Node initialized {0}Hz".format(frec))
         while not rospy.is_shutdown():
             if self.image.size > 0:
-                # process img
-                self.process_img()
-                # transform img to ROS Image msg
+                self.process_img()  # process img
+                # img to ROS Image msg
                 img_back = self.bridge.cv2_to_imgmsg(self.p_img)
-                # publish image
-                self.img_pub.publish(img_back)
+                self.img_pub.publish(img_back)  # publish image
             r.sleep()
 
     def process_img(self):
         hsv = cv2.cvtColor(self.image, cv2.COLOR_BGR2HSV)
-        # red mask
-        maskr = cv2.inRange(hsv, (0, 88, 179), (33, 255, 255))
-        # green mask
-        maskg = cv2.inRange(hsv, (49, 39, 130), (98, 255, 255))
-        # combine masks
-        mask = cv2.bitwise_or(maskg, maskr)
+        # mask
+        maskr = cv2.inRange(hsv, (0, 88, 179), (33, 255, 255))  # red mask
+        maskg = cv2.inRange(hsv, (49, 39, 130), (98, 255, 255))  # green mask
+        mask = cv2.bitwise_or(maskg, maskr)  # combine masks
         # apply mask to original image
         target = cv2.bitwise_and(self.image, self.image, mask=mask)
+
         # threshold
         ret, thresh = cv2.threshold(target, 127, 255, cv2.THRESH_BINARY)
 
-        # TODO:
-        # Remove noise w/erode o dilate
-        # detect blobs "simple_blob_detector"
+        # erode
+        kernel = np.ones((5, 5), np.uint8)
+        eroded = cv2.erode(thresh, kernel)
 
-        self.p_img = target
+        # blob detection
+        # Set up the detector with default parameters.
+        detector = cv2.SimpleBlobDetector()
+        # keypoints = detector.detect(eroded) # ERROR: segmentation fault
+
+        # result
+        self.p_img = eroded
 
     def img_cb(self, msg):
         self.image = self.bridge.imgmsg_to_cv2(msg)
