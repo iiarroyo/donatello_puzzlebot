dd2347564ae1619b8a660ca323e59d2165ac80a7
iiarroyo <israel.iap@outlook.com>
2022-04-28 17:37:33 -0500

    Color detection works

--- src/read_img.py
+++ src/read_img.py
@@ -23,36 +23,42 @@ class Observer():
             if self.image.size > 0:
                 self.process_img()  # process img
                 # img to ROS Image msg
-                img_back = self.bridge.cv2_to_imgmsg(self.p_img)
+                img_back = self.bridge.cv2_to_imgmsg(self.p_img, encoding="passthrough")
                 self.img_pub.publish(img_back)  # publish image
             r.sleep()
 
     def process_img(self):
         hsv = cv2.cvtColor(self.image, cv2.COLOR_BGR2HSV)
         # mask
-        maskr = cv2.inRange(hsv, (0, 88, 179), (33, 255, 255))  # red mask
+        # maskr = cv2.inRange(hsv, (0, 88, 179), (33, 255, 255))  # red mask
         maskg = cv2.inRange(hsv, (49, 39, 130), (98, 255, 255))  # green mask
-        mask = cv2.bitwise_or(maskg, maskr)  # combine masks
+        mask = maskg  # combine masks
         # apply mask to original image
         target = cv2.bitwise_and(self.image, self.image, mask=mask)
 
         # threshold
-        ret, thresh = cv2.threshold(target, 127, 255, cv2.THRESH_BINARY)
+        imgray = cv2.cvtColor(target, cv2.COLOR_BGR2GRAY)
+        ret, thresh = cv2.threshold(imgray, 200, 255, cv2.THRESH_BINARY)
 
         # erode
         kernel = np.ones((5, 5), np.uint8)
         eroded = cv2.erode(thresh, kernel)
+        eroded = np.uint8(eroded)
 
         # blob detection
         # Set up the detector with default parameters.
-        detector = cv2.SimpleBlobDetector()
-        # keypoints = detector.detect(eroded) # ERROR: segmentation fault
+        detector = cv2.SimpleBlobDetector_create()
+        keypoints = detector.detect(eroded) # ERROR: segmentation fault
 
         # result
         self.p_img = eroded
+        if len(keypoints) > 1 and keypoints[0].size > 1:
+            return True
+        else:
+            return False
 
     def img_cb(self, msg):
-        self.image = self.bridge.imgmsg_to_cv2(msg)
+        self.image = self.bridge.imgmsg_to_cv2(msg, desired_encoding="bgr8")
 
 
 if __name__ == "__main__":
