ced674785614cad383d4501b5e0970695a8a8a0c
iiarroyo <israel.iap@outlook.com>
2022-05-02 18:58:36 -0500

    HOLA

--- src/read_img.py
+++ src/read_img.py
@@ -12,7 +12,7 @@ class Observer():
         self.image = np.zeros((0, 0))  # subscriber image
         self.p_img = np.zeros((0, 0))  # processed image
         self.bridge = cv_bridge.CvBridge()  # cv_bridge
-        # rospy.Subscriber("/camera/image_raw", Image, self.img_cb)
+        # rospy.Subscriber("/video_source/raw", Image, self.img_cb)
         rospy.Subscriber("/usb_cam/image_raw", Image, self.img_cb)
         # image publisher
         self.img_pub = rospy.Publisher("holahola", Image, queue_size=10)
@@ -29,7 +29,7 @@ class Observer():
                 if color == "RED":
                     cmd.linear.x = 0.0
                 elif color == "GREEN":
-                    cmd.linear.x = 0.1
+                    cmd.linear.x = 0.3
                 else:
                     cmd.linear.x = 0.0
                 print(cmd)
@@ -40,20 +40,35 @@ class Observer():
             r.sleep()
 
     def detect_color(self, show_img=False):
-        hsv = cv2.cvtColor(self.image, cv2.COLOR_BGR2HSV)
+        image = self.image.copy()
+        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
         # mask
-        maskr = cv2.inRange(hsv, (0, 88, 179), (33, 255, 255))  # red mask
+        lowerr1 = np.array([0, 100, 20])
+        uperr1 = np.array([10, 255, 255])
+
+        lowerr2 = np.array([168, 100, 20])
+        uperr2 = np.array([179, 255, 255])
+
+        # maskr1 = cv2.inRange(hsv, lowerr1, uperr1)  # red mask
+        # maskr2 = cv2.inRange(hsv, lowerr2, uperr2)  # red mask
+        # maskr = maskr1 + maskr2
+        # maskr = cv2.inRange(hsv, (0, 88, 179), (33, 255, 255))
+        maskr = cv2.inRange(hsv, (70, 100, 179), (120, 255, 255))
+
+
+        # maskg = cv2.inRange(hsv, (49, 39, 130), (98, 255, 255))  # green mask
         maskg = cv2.inRange(hsv, (49, 39, 130), (98, 255, 255))  # green mask
+
         # apply mask to original image
-        targetr = cv2.bitwise_and(self.image, self.image, mask=maskr)
-        targetg = cv2.bitwise_and(self.image, self.image, mask=maskg)
+        targetr = cv2.bitwise_and(image, image, mask=maskr)
+        targetg = cv2.bitwise_and(image, image, mask=maskg)
 
 
         # threshold
         imgrayr = cv2.cvtColor(targetr, cv2.COLOR_BGR2GRAY)
         imgrayg = cv2.cvtColor(targetg, cv2.COLOR_BGR2GRAY)
-        ret, threshr = cv2.threshold(imgrayr, 200, 255, cv2.THRESH_BINARY)
-        ret, threshg = cv2.threshold(imgrayg, 200, 255, cv2.THRESH_BINARY)
+        ret, threshr = cv2.threshold(imgrayr, 100, 255, cv2.THRESH_BINARY)
+        ret, threshg = cv2.threshold(imgrayg, 100, 255, cv2.THRESH_BINARY)
 
         # erode
         kernel = np.ones((5, 5), np.uint8)
@@ -68,7 +83,7 @@ class Observer():
         keypointsg = detector.detect(erodedg)
 
         # result
-        self.p_img = erodedg
+        self.p_img = erodedr
         blob_sizesr = [blob.size > 1.0 for blob in keypointsr]
         blob_sizesg = [blob.size > 1.0 for blob in keypointsg]
 
@@ -80,7 +95,7 @@ class Observer():
             return None
 
     def img_cb(self, msg):
-        self.image = self.bridge.imgmsg_to_cv2(msg, desired_encoding="bgr8")
+        self.image = self.bridge.imgmsg_to_cv2(msg)
 
     def cleanup(self):
         pass
