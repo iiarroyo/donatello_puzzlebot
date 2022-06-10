5fd6c133dd458b907e6bcd72957d224baf68b1cd
iiarroyo <israel.iap@outlook.com>
2022-06-10 16:51:05 -0500

    Added PI

--- src/detect_line.py
+++ src/detect_line.py
@@ -13,17 +13,18 @@ from sensor_msgs.msg import Image
 from geometry_msgs.msg import Twist
 from matplotlib import cm, pyplot as plt
 
+ki = 0.01
+kp = 0.003
 
 class Observer():
     def __init__(self):
         if platform.machine() == 'x86_64':
             reset_simulation = rospy.ServiceProxy(
-                            '/gazebo/reset_simulation', Empty)
+                '/gazebo/reset_simulation', Empty)
             reset_simulation()
             rospy.Subscriber("/camera/image_raw", Image, self.img_cb)
         else:
             rospy.Subscriber("/video_source/raw", Image, self.img_cb)
-        
 
         self.image = None  # subscriber image
         self.p_img = np.zeros((0, 0))  # processed image
@@ -38,7 +39,7 @@ class Observer():
         r = rospy.Rate(frec)  # Hz
         print("Node initialized {0}Hz".format(frec))
         cmd = Twist()
-        prev_line = 0
+        e = [0.0, 0.0, 0.0]  # error list
         while not rospy.is_shutdown():
             if self.image is None:
                 print("image is None")
@@ -47,14 +48,16 @@ class Observer():
             self.line_pub.publish(line)
             if line is not None:
                 print("Line: ", abs(48-line))
-                if(abs(48-line)<10):
+                if(abs(48-line) < 10):
                     cmd.linear.x = (0.125)
                 else:
                     cmd.linear.x = (0.05)
+                e.insert(0, 48 - line)
+                e.pop()
+                cmd.angular.z = kp * e[0] + ki * e[2]
 
-                cmd.angular.z = 0.003 * (48 - line)
-                print("angular", cmd.angular.z )
-                print("linear", cmd.linear.x)
+                # print("angular", cmd.angular.z)
+                # print("linear", cmd.linear.x)
 
             else:
                 cmd.linear.x = 0.0
@@ -62,42 +65,42 @@ class Observer():
             self.cmd_pub.publish(cmd)
             # img to ROS Image msg
             if self.p_img is not None:
-                img_back = self.bridge.cv2_to_imgmsg(self.p_img, encoding="passthrough")
+                img_back = self.bridge.cv2_to_imgmsg(
+                    self.p_img, encoding="passthrough")
                 self.img_pub.publish(img_back)  # publish image
             r.sleep()
 
-    def detect_line(self, prev_line=None,plot=False):
+    def detect_line(self, prev_line=None, plot=False):
         image = self.image.copy()
-        scale_percent = 20 # percent of original size
+        scale_percent = 20  # percent of original size
         width = int(image.shape[1] * scale_percent / 100)
         height = int(image.shape[0] * scale_percent / 100)
         dim = (width, height)
         # resize image
         img_height, img_width, _ = image.shape
         # resized = cv2.resize(image, dim, interpolation = cv2.INTER_AREA)
-        gray_resized =  cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        gray_resized = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         gray_resized_blur = cv2.GaussianBlur(gray_resized, (7, 7), 0)
-        cropped = gray_resized_blur[int(img_height*0.70):, int(img_width*0.35):-int(img_width*0.35)]
+        cropped = gray_resized_blur[int(
+            img_height*0.70):, int(img_width*0.35):-int(img_width*0.35)]
         # print(cropped.shape)
-        ret,thresh = cv2.threshold(cropped, 100, 255, cv2.THRESH_BINARY)
+        ret, thresh = cv2.threshold(cropped, 100, 255, cv2.THRESH_BINARY)
         # print(type(thresh))
         kernel = np.ones((6, 6), np.uint8)
         eroded = cv2.erode(~thresh, kernel)
         col_sum = np.sum(eroded, axis=0, dtype="int64")
         col_sum_l = col_sum[:int(len(col_sum)*0.5)]
         col_sum_r = col_sum[int(len(col_sum)*0.5):]
-        #print(sum(col_sum_l))
-        #print(sum(col_sum_r))
-        dif=sum(col_sum_l)-sum(col_sum_r)
+        # print(sum(col_sum_l))
+        # print(sum(col_sum_r))
+        dif = sum(col_sum_l)-sum(col_sum_r)
         # print("dif", dif)
-
 
         self.p_img = eroded  # processed image
 
-
         avg = np.mean(col_sum)
         #print("avg", avg)
-        
+
         # if plot:
         # plt.figure(1)
         # x = np.arange(len(col_sum))
@@ -117,12 +120,10 @@ class Observer():
         for i in range(0, len(arr)//win_size, win_size):
             win_sum = 0
             for j in range(i, i+win_size, 1):
-                win_sum+=arr[j]
+                win_sum += arr[j]
                 arr[j] = 0
             arr[i] = win_sum
         return arr
-            
-
 
     def img_cb(self, msg):
         self.image = self.bridge.imgmsg_to_cv2(msg, desired_encoding="bgr8")
@@ -131,9 +132,8 @@ class Observer():
     def cleanup(self):
         last_cmd = Twist()
         self.cmd_pub.publish(last_cmd)
-    
+
 
 if __name__ == "__main__":
     rospy.init_node("img_detector", anonymous=True)
     Observer()
-
